0d8b8bcd9edb04e0c3e15bc98e09b1e0ac9cb96c
Paul Lewis <github@aerotwist.com>
2011-09-06 23:27:32 +0200

    Tidied up the woeful formatting. Changing IDEs half way through a project is bad.

--- src/js/core/objects/primitives/Cube.js
+++ src/js/core/objects/primitives/Cube.js
@@ -1,63 +1,63 @@
 /**
  * @class Represents a cube primitive
- * 
+ *
  * @augments A3.Core.Object3D
  * @author Paul Lewis
- * 
+ *
  * @param {Number} size The size of the cube in all three dimensions
  */
 A3.Core.Objects.Primitives.Cube = function(size) {
 
 	size *= 0.5;
-	
+
 	var vertices  = null,
 		faces       = null,
 		colors      = [],
 		c           = 0,
-		
+
 		fBL	= new A3.Vertex(-size, -size,  size), // 0
 		fBR	= new A3.Vertex( size, -size,  size), // 1
 		fTR	= new A3.Vertex( size,  size,  size), // 2
 		fTL	= new A3.Vertex(-size,  size,  size), // 3
-		
+
 		bBR	= new A3.Vertex( size, -size, -size), // 4
 		bBL	= new A3.Vertex(-size, -size, -size), // 5
 		bTL	= new A3.Vertex(-size,  size, -size), // 6
 		bTR	= new A3.Vertex( size,  size, -size); // 7
-		
+
 	vertices = [
-		fBL,							fBR,							fTR,							fTL,
+		fBL,								fBR,							fTR,							fTL,
 		bBR,							bBL,							bTL,							bTR,
 		(new A3.Vertex()).copy(bBL),	(new A3.Vertex()).copy(fBL),	(new A3.Vertex()).copy(fTL),	(new A3.Vertex()).copy(bTL),
 		(new A3.Vertex()).copy(fBR),	(new A3.Vertex()).copy(bBR),	(new A3.Vertex()).copy(bTR),	(new A3.Vertex()).copy(fTR),
 		(new A3.Vertex()).copy(fTR),	(new A3.Vertex()).copy(bTR),	(new A3.Vertex()).copy(bTL),	(new A3.Vertex()).copy(fTL),
 		(new A3.Vertex()).copy(fBR),	(new A3.Vertex()).copy(fBL),	(new A3.Vertex()).copy(bBL),	(new A3.Vertex()).copy(bBR)
 	];
-	
+
 	faces = [
 		// front
 		new A3.Face4(0, 1, 2, 3),
-		
+
 		// back
 		new A3.Face4(4, 5, 6, 7),
-		
+
 		// left
 		new A3.Face4(8, 9, 10,11),
-		
+
 		// right
 		new A3.Face4(12,13,14,15),
-		
+
 		// top
 		new A3.Face4(16,17,18,19),
-		
+
 		// bottom
 		new A3.Face4(20,21,22,23)
 	];
-	
+
 	for(c = 0; c < vertices.length; c++) {
 	  colors.push(new A3.V3(1,1,1));
 	}
-	
+
 	// convert to a geometry object
 	return new A3.Geometry({
 	  vertices: vertices,
--- src/js/core/objects/primitives/Sphere.js
+++ src/js/core/objects/primitives/Sphere.js
@@ -1,155 +1,155 @@
 /**
  * @class Represents a sphere primitive
- * 
+ *
  * @augments A3.Core.Object3D
  * @author Paul Lewis
- * 
+ *
  * @param {Number} radius The radius of the sphere
  * @param {Number} segments The number of vertical slices, analagous to latitude
  * @param {Number} rings The number of horizontal slices, analagous to longitude
  */
 A3.Core.Objects.Primitives.Sphere = function(radius, segments, rings) {
 
-	var MIN_THETA	  = Math.PI * -0.5,
-		MAX_THETA	    = Math.PI * 0.5,
-		RANGE_THETA	  = Math.PI,
-		MIN_PHI		    = 0,
-		MAX_PHI		    = Math.PI * 2,
-		
-		vertices	    = [],
-		faces		      = [],
-    uvs           = [],
-		colors		    = [],
-		
-		ring		      = 0,
-		segment		    = 0,
-		theta		      = MIN_THETA,
-		phi			      = MIN_PHI,
-		nextTheta	    = 0,
-		nextPhi	      = 0,
-		face          = null,
-		v1            = null,
-		v2            = null,
-		v3            = null,
-		v4            = null;
-		
-	rings          = Math.max(rings, 3);
-	segments       = Math.max(segments, 3);
-		
+	var MIN_THETA			= Math.PI * -0.5,
+		MAX_THETA		= Math.PI * 0.5,
+		RANGE_THETA		= Math.PI,
+		MIN_PHI			= 0,
+		MAX_PHI			= Math.PI * 2,
+
+		vertices				= [],
+		faces				= [],
+		uvs					= [],
+		colors				= [],
+
+		ring				= 0,
+		segment			= 0,
+		theta				= MIN_THETA,
+		phi					= MIN_PHI,
+		nextTheta			= 0,
+		nextPhi				= 0,
+		face				= null,
+		v1					= null,
+		v2					= null,
+		v3					= null,
+		v4					= null;
+
+	rings					= Math.max(rings, 3);
+	segments				= Math.max(segments, 3);
+
 	function createData(theta, phi) {
-		var cosTheta = Math.cos(theta),
-			sinTheta	 = Math.sin(theta),
-			cosPhi		 = Math.cos(phi),
-			sinPhi		 = Math.sin(phi);
-			
+		var cosTheta		= Math.cos(theta),
+			sinTheta		= Math.sin(theta),
+			cosPhi			= Math.cos(phi),
+			sinPhi			= Math.sin(phi);
+
 			x = cosTheta * cosPhi * radius;
 			y = -sinTheta * radius;
 			z = -cosTheta * sinPhi * radius;
-			
+
 		return compareToExisting({
 			vertex: new A3.Vertex(Math.round(x), Math.round(y), Math.round(z)),
 			color: new A3.V3(1.0,1.0,1.0)
 		});
 	}
-	
+
 	function compareToExisting(data) {
-		
-		var vertexPosToCompare = null,
-			  exists             = false,
-			  index              = 0,
-			  v                  = 0;
-		
+
+		var vertexPosToCompare	= null,
+			exists					= false,
+			index					= 0,
+			v						= 0;
+
 		for(v = 0; v < vertices.length; v++) {
 			vertexPosToCompare = vertices[v].position;
-			exists = ((vertexPosToCompare.x === data.vertex.position.x) && 
-						    (vertexPosToCompare.y === data.vertex.position.y) && 
+			exists = ((vertexPosToCompare.x === data.vertex.position.x) &&
+						    (vertexPosToCompare.y === data.vertex.position.y) &&
 						    (vertexPosToCompare.z === data.vertex.position.z) );
-						
+
 			if(exists) {
 				index = v;
 				break;
 			}
 		}
-		
+
 		if(!exists) {
 			vertices.push(data.vertex);
 			colors.push(data.color);
-			
+
 			index = vertices.length-1;
 		}
-		
+
 		return index;
 	}
-	
+
 	function toFaceUV(theta, phi) {
 		return new A3.V2(phi / MAX_PHI, 1-((theta / MAX_THETA) + 1) * 0.5);
 	}
 
 	// run through
 	while(ring < rings) {
-		
-		phi		   = (ring / rings) * MAX_PHI;
-		nextPhi  = ((ring+1) / rings) * MAX_PHI;
-		segment  = 0;
-		
+
+		phi			= (ring / rings) * MAX_PHI;
+		nextPhi		= ((ring+1) / rings) * MAX_PHI;
+		segment	= 0;
+
 		while(segment < segments) {
-			
-			theta		  = MIN_THETA + (segment / segments) * RANGE_THETA;
+
+			theta		= MIN_THETA + (segment / segments) * RANGE_THETA;
 			nextTheta	= MIN_THETA + ((segment+1) / segments) * RANGE_THETA;
-			
+
 			v1 = createData(theta, phi);
 			v2 = createData(nextTheta, phi);
 			v3 = createData(nextTheta, nextPhi);
 			v4 = createData(theta, nextPhi);
-			
+
 			// now check for a triangle rather than a quad
 			if(v1 === v2 || v2 === v3 || v3 === v4 || v4 === v1) {
-			  
+
 				if(v1 === v2) {
-				  
+
 					face = new A3.Face3(v1, v3, v4);
-					uvs.push(toFaceUV(theta, phi));			     // v1
-					uvs.push(toFaceUV(nextTheta, nextPhi));  // v3
-					uvs.push(toFaceUV(theta, nextPhi));      // v4
-					
+					uvs.push(toFaceUV(theta, phi));			// v1
+					uvs.push(toFaceUV(nextTheta, nextPhi));	// v3
+					uvs.push(toFaceUV(theta, nextPhi));		// v4
+
 				} else if(v2 === v3) {
-				  
+
 					face = new A3.Face3(v1, v2, v4);
-					uvs.push(toFaceUV(theta, phi));          // v1
-					uvs.push(toFaceUV(nextTheta, phi));      // v2
-					uvs.push(toFaceUV(theta, nextPhi));      // v4
-					
+					uvs.push(toFaceUV(theta, phi));			// v1
+					uvs.push(toFaceUV(nextTheta, phi));		// v2
+					uvs.push(toFaceUV(theta, nextPhi));		// v4
+
 				} else if(v3 === v4) {
-				  
+
 					face = new A3.Face3(v1, v2, v4);
-					uvs.push(toFaceUV(theta, phi));          // v1
-					uvs.push(toFaceUV(nextTheta, phi));      // v2
-					uvs.push(toFaceUV(theta, nextPhi));      // v4
-					
+					uvs.push(toFaceUV(theta, phi));			// v1
+					uvs.push(toFaceUV(nextTheta, phi));		// v2
+					uvs.push(toFaceUV(theta, nextPhi));		// v4
+
 				} else if(v4 === v1) {
-				  
+
 					face = new A3.Face3(v1, v2, v3);
-					uvs.push(toFaceUV(theta, phi));          // v1
-					uvs.push(toFaceUV(nextTheta, phi));      // v2
-					uvs.push(toFaceUV(nextTheta, nextPhi));  // v3
-					
+					uvs.push(toFaceUV(theta, phi));			// v1
+					uvs.push(toFaceUV(nextTheta, phi));		// v2
+					uvs.push(toFaceUV(nextTheta, nextPhi));	// v3
+
 				}
 			} else {
-			  
+
 				face = new A3.Face4(v1, v2, v3, v4);
-				uvs.push(toFaceUV(theta, phi));            // v1
-				uvs.push(toFaceUV(nextTheta, phi));        // v2
-				uvs.push(toFaceUV(nextTheta, nextPhi));    // v3
-				uvs.push(toFaceUV(theta, nextPhi));        // v4
-				
+				uvs.push(toFaceUV(theta, phi));				// v1
+				uvs.push(toFaceUV(nextTheta, phi));			// v2
+				uvs.push(toFaceUV(nextTheta, nextPhi));		// v3
+				uvs.push(toFaceUV(theta, nextPhi));			// v4
+
 			}
-			
+
 			faces.push(face);
 			segment++;
 		}
 		ring++;
 	}
-	
+
 	// convert to a geometry object
 	return new A3.Geometry({
 		vertices: vertices,
